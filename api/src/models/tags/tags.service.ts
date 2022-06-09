1d2c329c038d9862b0c531dc6531e877defe1e0d
ElayadeIsmail <elayadeismail@gmail.com>
2022-06-09 22:12:18 +0100

    add comments to tag service

--- api/src/models/tags/tags.service.ts
+++ api/src/models/tags/tags.service.ts
@@ -15,34 +15,45 @@ export class TagsService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create({ name }: { name: string }): Promise<Tag> {
+    // check if name already exist
     const nameAlreadyExist = await this.prisma.tag.findUnique({
       where: { name },
     });
+    // return 400 if name already exist
     if (nameAlreadyExist) {
       throw new BadRequestException('Name already exist');
     }
+    // create new record and return it
     return this.prisma.tag.create({ data: { name } });
   }
 
   async findOne(id: number): Promise<Tag> {
+    //   find tag by ID
     const tag = await this.prisma.tag.findUnique({ where: { id } });
+    // return 404 if tag not found
     if (!tag) {
       throw new NotFoundException('Tag not found');
     }
+    // return 200 and record
     return tag;
   }
 
   async find(findManyArgs: FindManyTagsInputs): Promise<IPagination<Tag>> {
     let cursor: Prisma.TagWhereUniqueInput;
     let skip: number;
+    // make sure limit is less than PAGINATION_LIMIT_MAX = 50
     const limit = Math.min(findManyArgs.limit, PAGINATION_LIMIT_MAX);
+    // add one to limit so we can check if there is more
+    // items in the database for another request
     const limitPlusOne = limit + 1;
+    // check if cursor not null
     if (findManyArgs.cursor) {
       cursor = {
         id: findManyArgs.cursor,
       };
       skip = 1;
     }
+    // get items sorted by id DESC
     const items = await this.prisma.tag.findMany({
       where: {
         name: {
@@ -57,6 +68,9 @@ export class TagsService {
         id: 'desc',
       },
     });
+    // return items with less item
+    // because we added one to check if there is more items
+    // for another pagination
     return {
       items: items.slice(0, limit),
       hasMore: items.length === limitPlusOne,
@@ -64,6 +78,9 @@ export class TagsService {
     };
   }
   async update(id: number, name: string): Promise<Tag> {
+    //   get item by ID
+    // and check if there is another item with the new name
+    // that we're trying to insert
     const [tag, newNameAlreadyExist] = await Promise.all([
       this.prisma.tag.findMany({ where: { id } }),
       this.prisma.tag.findFirst({
@@ -75,20 +92,26 @@ export class TagsService {
         },
       }),
     ]);
+    // return 400 if there is no item
     if (!tag) {
       throw new BadRequestException('tag not found');
     }
+    // return 400 if the new name already taken by another tag
     if (newNameAlreadyExist) {
       throw new BadRequestException('New Name already exist');
     }
+    // update name of tag and return the updated item
     return this.prisma.tag.update({ where: { id }, data: { name } });
   }
   async remove(id: number): Promise<Tag> {
     // TODO Check if tag still connected with products
+    // get item by ID
     const tag = await this.prisma.tag.findUnique({ where: { id } });
+    // return 400 if item does not exist
     if (!tag) {
       throw new BadRequestException('tag not found');
     }
+    // delete item and return it
     return this.prisma.tag.delete({ where: { id } });
   }
 }
