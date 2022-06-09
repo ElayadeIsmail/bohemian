dcd387bfee326b81aa29e09181664ad2fe6a53cc
ElayadeIsmail <elayadeismail@gmail.com>
2022-06-09 22:01:57 +0100

    add remove and update tag services

--- api/src/common/constants.ts
+++ api/src/common/constants.ts
@@ -1,1 +1,2 @@
 export const PAGINATION_LIMIT = 20;
+export const PAGINATION_LIMIT_MAX = 50;
--- api/src/models/tags/tags.service.ts
+++ api/src/models/tags/tags.service.ts
@@ -4,6 +4,7 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
+import { PAGINATION_LIMIT_MAX } from 'src/common/constants';
 import { PrismaService } from 'src/common/services/prisma/prisma.service';
 import { IPagination } from 'src/common/types/pagination';
 import { FindManyTagsInputs } from './inputs/find-many-tags-args';
@@ -34,7 +35,8 @@ export class TagsService {
   async find(findManyArgs: FindManyTagsInputs): Promise<IPagination<Tag>> {
     let cursor: Prisma.TagWhereUniqueInput;
     let skip: number;
-    const limitPlusOne = findManyArgs.limit + 1;
+    const limit = Math.min(findManyArgs.limit, PAGINATION_LIMIT_MAX);
+    const limitPlusOne = limit + 1;
     if (findManyArgs.cursor) {
       cursor = {
         id: findManyArgs.cursor,
@@ -56,9 +58,37 @@ export class TagsService {
       },
     });
     return {
-      items: items.slice(0, findManyArgs.limit),
+      items: items.slice(0, limit),
       hasMore: items.length === limitPlusOne,
       cursor: items[items.length - 1].id,
     };
+  }
+  async update(id: number, name: string): Promise<Tag> {
+    const [tag, newNameAlreadyExist] = await Promise.all([
+      this.prisma.tag.findMany({ where: { id } }),
+      this.prisma.tag.findFirst({
+        where: {
+          name,
+          id: {
+            not: { equals: id },
+          },
+        },
+      }),
+    ]);
+    if (!tag) {
+      throw new BadRequestException('tag not found');
+    }
+    if (newNameAlreadyExist) {
+      throw new BadRequestException('New Name already exist');
+    }
+    return this.prisma.tag.update({ where: { id }, data: { name } });
+  }
+  async remove(id: number): Promise<Tag> {
+    // TODO Check if tag still connected with products
+    const tag = await this.prisma.tag.findUnique({ where: { id } });
+    if (!tag) {
+      throw new BadRequestException('tag not found');
+    }
+    return this.prisma.tag.delete({ where: { id } });
   }
 }
